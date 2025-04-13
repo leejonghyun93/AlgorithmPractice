a35c3d95fded776c2afa09a78aad83937ab4289a
leejonghyun93 <fhohffodf93@gmail.com>
2025-04-13 19:34:58 +0900

    문제 65

---
+++ src/solution/Solution65.java
@@ -0,0 +1,18 @@
+package solution;
+
+
+/* 문제 :  */
+
+
+class Solution65 {
+
+    public int solution(int n) {
+        return Integer.toBinaryString(n).replace("0","").length();
+    }
+
+}
+
+
+/*
+ *
+ **/
